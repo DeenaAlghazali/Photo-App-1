c7fc7b342997ab888467d9c9a63fcedca957a1b1
WessalJawad95 <wessaljawad95@gmail.com>
2022-02-22 19:48:45 +0200

    handle data from api relates #7

--- src/app.js
+++ src/app.js
@@ -1,28 +1,36 @@
-const express = require('express');
-const helmet = require('helmet');
-const path = require('path');
-const fetch = require('node-fetch');
+const express = require("express");
+const helmet = require("helmet");
+const path = require("path");
+const fetch = require("node-fetch");
+
 
 const app = express();
 app.use(helmet());
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-app.use(express.static(path.join(__dirname, '..', 'public')));
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
-})
-app.get('/search', (req, res) =>{
-    fetch('https://api.unsplash.com/search/photos?query=$%7Bquery%7D&page=$%7Bpage%7D&client_id=7hTcanzZHuFtZWDwYJxn_q1NcwLueIkKwBMlqLrnQVM')
-    .then(response => {
-        return response.json()
-    }).then(data =>{
-         res.json(data);
-        })
-    .catch ((err) => {
-        res.send(err);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
+app.post("/search", (req, res) => {
+  
+  const query=req.body
+  console.log(req.body)
+  const page = 1
+  fetch(
+    `https://api.unsplash.com/search/photos?query=${query}&page=${page}&client_id=7hTcanzZHuFtZWDwYJxn_q1NcwLueIkKwBMlqLrnQVM`
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.send(err);
     });
 });
-app.set('PORT', 2220);
-app.listen(app.get('PORT'), ()=> {
-    console.log(`http://localhost:${app.get('PORT')}`);
+app.set("PORT", 2220);
+app.listen(app.get("PORT"), () => {
+  console.log(`http://localhost:${app.get("PORT")}`);
 });
