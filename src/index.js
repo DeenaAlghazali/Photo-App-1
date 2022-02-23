9d2d63d6531c81aa97efe2db3b81eb1f3a125c3a
=DeenaAlghazali <deenaalghazali11@gmail.com>
2022-02-23 16:06:19 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 require('env2')('.env');
 
-app.set('port', 2220 || process.env.PORT);
+app.set('port', process.env.PORT || 2220);
 
 app.listen(app.get('port'), () => {
   console.log(`http://localhost:${app.get('port')}`);
